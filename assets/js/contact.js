ff3dfcbc5c41c5968c73fb753fb171c541f5dacf
jmoon0 <moonjoshua3@gmail.com>
2023-10-17 09:04:06 -0500

    added contact form functionality

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+/*const submitButton = document.querySelector(".button.default.submit");
+
+submitButton.addEventListener('click', clickSubmit); */
+
+//TODO: add button functionality (dialog that confirms submission)
+//function clickSubmit()
